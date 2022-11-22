e4e3140a31f8d42abe86c0f455180f2e9c35c3ff
cmstorma1989 <117497766+cmstorma1989@users.noreply.github.com>
2022-11-22 17:13:44 -0500

    .

--- Project3/Project3/DataSet1.vb
+++
@@ -1,8 +0,0 @@
-﻿Partial Class DataSet1
-    Partial Public Class EmpresaDataTable
-        Private Sub EmpresaDataTable_EmpresaRowChanging(sender As Object, e As EmpresaRowChangeEvent) Handles Me.EmpresaRowChanging
-
-        End Sub
-
-    End Class
-End Class
--- Project3/Project3/DataSet2.vb
+++
@@ -1,8 +0,0 @@
-﻿Partial Class DataSet2
-    Partial Public Class OficinaDataTable
-        Private Sub OficinaDataTable_OficinaRowChanging(sender As Object, e As OficinaRowChangeEvent) Handles Me.OficinaRowChanging
-
-        End Sub
-
-    End Class
-End Class
